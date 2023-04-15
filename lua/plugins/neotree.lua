fbb3343f85661fcf104a6ba4d194f47c7e9f4320
Shad Kaske <shadkaske@gmail.com>
2023-04-15 16:20:52 -0500

    Lazy Load NeoTree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,12 +1,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  lazy = true,
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
-  end,
+  opts = {},
+  cmd = {
+    "NeoTreeFocusToggle"
+  },
 }
