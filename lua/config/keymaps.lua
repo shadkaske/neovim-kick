7a28ddcbf22fd39d485a73e4ebd17892d0c0093d
Shad Kaske <shadkaske@gmail.com>
2023-04-15 16:45:48 -0500

    Separated LspConfigs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,11 @@
 local wk = require("which-key")
 
 -- Don't Override Leader
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Visual Indent Lines
 vim.keymap.set("v", ">", ">gv")
@@ -26,6 +26,10 @@ vim.keymap.set("n", ";;", "A;<ESC>")
 
 -- Toggle NeoTree
 vim.keymap.set({"n", "v" }, "<leader>e", "<cmd>NeoTreeFocusToggle<cr>", { desc = "NeoTree Toggle" })
+
+-- Lsp Keymaps
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
 
 -- Which key labels
 wk.register(
