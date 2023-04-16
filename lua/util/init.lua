220bfcad55a20047ff4435e4c73e4a2d5b216376
Shad Kaske <shadkaske@gmail.com>
2023-04-16 11:03:52 -0500

    Utility Functions file added

---
+++ lua/util/init.lua
@@ -0,0 +1,9 @@
+-- Utility functions to use in multiple places
+local M = {}
+
+function M.plugin_loaded(plugin)
+  local lazy_config_avail, lazy_config = pcall(require, "lazy.core.config")
+  return lazy_config_avail and lazy_config.plugins[plugin] ~= nil
+end
+
+return M
