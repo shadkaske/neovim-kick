3f7d44236f4a5336aa8bc2b2cfc8bc58cc8f919c
Shad Kaske <shadkaske@gmail.com>
2023-04-16 08:52:43 -0500

    remove whichkey groups symbol

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,5 +8,10 @@ return {
       align = "center",
       spacing = 4,
     },
+    icons = {
+      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+      separator = "➜", -- symbol used between a key and it's label
+      group = "", -- symbol prepended to a group
+    },
   }
 }
