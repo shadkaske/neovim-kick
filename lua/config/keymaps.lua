3aa1e62ea5e40ac29e405c193e14c684e84bdb38
Shad Kaske <shadkaske@gmail.com>
2023-04-16 08:51:53 -0500

    Removing the default keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -25,54 +25,54 @@ vim.keymap.set("n", "]o", "o<ESC>")
 vim.keymap.set("n", ";;", "A;<ESC>")
 
 -- Toggle NeoTree
-vim.keymap.set({"n", "v" }, "<leader>e", "<cmd>NeoTreeFocusToggle<cr>", { desc = "NeoTree Toggle" })
+vim.keymap.set({ "n", "v" }, "<leader>e", "<cmd>NeoTreeFocusToggle<cr>", { desc = "NeoTree Toggle" })
 
 -- Lsp Keymaps
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
 vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
 
--- Kickstart maps
--- TODO: Remove These
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-
 -- Navigator Keymaps
-vim.keymap.set({'n', 't'}, '<C-h>', '<CMD>NavigatorLeft<CR>')
-vim.keymap.set({'n', 't'}, '<C-l>', '<CMD>NavigatorRight<CR>')
-vim.keymap.set({'n', 't'}, '<C-k>', '<CMD>NavigatorUp<CR>')
-vim.keymap.set({'n', 't'}, '<C-j>', '<CMD>NavigatorDown<CR>')
-vim.keymap.set({'n', 't'}, '<C-p>', '<CMD>NavigatorPrevious<CR>')
+vim.keymap.set({ 'n', 't' }, '<C-h>', '<CMD>NavigatorLeft<CR>')
+vim.keymap.set({ 'n', 't' }, '<C-l>', '<CMD>NavigatorRight<CR>')
+vim.keymap.set({ 'n', 't' }, '<C-k>', '<CMD>NavigatorUp<CR>')
+vim.keymap.set({ 'n', 't' }, '<C-j>', '<CMD>NavigatorDown<CR>')
+vim.keymap.set({ 'n', 't' }, '<C-p>', '<CMD>NavigatorPrevious<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Which key labels
+local icons = require("util.icons")
+
 wk.register(
   {
-    y = { name = " 󰨸 Clipboard" },
-    b = { name = "  Buffers"},
-    g = { name = "  Git"},
+    b = { name = string.format("%s Buffers", icons.Buffers) },
+    f = { name = string.format("%s Find", icons.Search) },
+    g = { name = string.format("%s Git", icons.Git) },
+    s = { name = string.format("%s Session", icons.Session) },
+    y = { name = string.format("%s Clipboard", icons.Paste) },
   },
   {
     mode = { "n", "v" },
     prefix = "<leader>",
   }
 )
+
+-- f Prefix Find Keymaps
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Files" })
+vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "Help" })
+vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "Current Word" })
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "Diagnostics" })
+vim.keymap.set("n", "<leader>fr", require("telescope.builtin").oldfiles, { desc = "Recent Files" })
+vim.keymap.set("n", "<leader>fi", function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = "In Current Buffer" })
 
 -- y Prefix
 vim.keymap.set({ "n", "v" }, "<leader>yo", '"_dP', { desc = "Paste Over" })
@@ -89,5 +89,7 @@ vim.keymap.set({ "n", "v" }, "<leader>yd", '"+d', { desc = "Delete to System" })
 -- vim.keymap.set({ "n", "v" }, "<leader>bd", '"+d', { desc = "Delete to System" })
 
 -- g prefix ( git stuff )
--- ["<leader>gn"] = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", desc = "Next Hunk", },
--- ["<leader>gp"] = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", desc = "Previous Hunk", },
+vim.keymap.set("n", "<leader>gn", "<cmd>lua require('gitsigns').next_hunk({navigation_message=false})<cr>",
+{ desc = "Next Hunk" })
+vim.keymap.set("n", "<leader>gp", "<cmd>lua require('gitsigns').prev_hunk({navigation_message=false})<cr>",
+{ desc = "Previous Hunk" })
