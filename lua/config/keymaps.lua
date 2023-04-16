a64ae99dde18489a96b37709053e4e807a4a6da4
Shad Kaske <shadkaske@gmail.com>
2023-04-16 11:04:23 -0500

    Keymaps for git, lsp and removed treesitter maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,11 @@
 local wk = require("which-key")
+local util = require("util")
 
 -- Don't Override Leader
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Save buffer with C-s
+vim.keymap.set({ "n", "i" }, "<C-s>", ":w<cr>", { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -14,6 +18,10 @@ vim.keymap.set("v", "<", "<gv")
 -- Center view on jumps
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 
@@ -51,6 +59,7 @@ wk.register(
     f = { name = string.format("%s Find", icons.Search) },
     g = { name = string.format("%s Git", icons.Git) },
     l = { name = string.format("%s LSP", icons.LSP) },
+    p = { name = string.format("%s Plugins", icons.Package) },
     s = { name = string.format("%s Session", icons.Session) },
     y = { name = string.format("%s Clipboard", icons.Paste) },
   },
@@ -61,6 +70,7 @@ wk.register(
 )
 
 -- f Prefix Find Keymaps
+vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Files" })
 vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "Help" })
 vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "Current Word" })
@@ -82,15 +92,59 @@ vim.keymap.set({ "n", "v" }, "<leader>yy", '"+y', { desc = "Yank to System" })
 vim.keymap.set({ "n", "v" }, "<leader>yd", '"+d', { desc = "Delete to System" })
 
 -- b Prefix
--- Close Buffer
--- vim.keymap.set({ "n", "v" }, "<leader>bd", '"_dP', { desc = "Paste Over" })
--- Force Delete buffer
--- vim.keymap.set({ "n", "v" }, "<leader>bD", '"+p', { desc = "Paste System" })
--- vim.keymap.set({ "n", "v" }, "<leader>by", '"+y', { desc = "Yank to System" })
--- vim.keymap.set({ "n", "v" }, "<leader>bd", '"+d', { desc = "Delete to System" })
+vim.keymap.set({ "n" }, "<leader>bd", "<cmd>Bdelete<cr>", { desc = "Close Buffer" })
+vim.keymap.set({ "n" }, "<leader>bD", "<cmd>Bdelete<cr>", { desc = "Force Close Buffer" })
+vim.keymap.set({ "n" }, "<leader>bn", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
+vim.keymap.set({ "n" }, "<leader>bp", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Buffer" })
+vim.keymap.set({ "n" }, "<leader>bj", "<cmd>BufferLinePick<cr>", { desc = "Jump to Buffer" })
+vim.keymap.set("n", "<leader>bf", require("telescope.builtin").buffers, { desc = "Buffers" })
+
+-- Buffer related
+vim.keymap.set({ "n" }, "<leader>C", "<cmd>Bdelete!<cr>", { desc = "Force Close Buffer" })
+vim.keymap.set({ "n" }, "<leader>c", "<cmd>Bdelete!<cr>", { desc = "Close Buffer" })
+vim.keymap.set({ "n" }, "H", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
+vim.keymap.set({ "n" }, "L", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
 
 -- g prefix ( git stuff )
 vim.keymap.set("n", "<leader>gn", "<cmd>lua require('gitsigns').next_hunk({navigation_message=false})<cr>",
-{ desc = "Next Hunk" })
+  { desc = "Next Hunk" })
 vim.keymap.set("n", "<leader>gp", "<cmd>lua require('gitsigns').prev_hunk({navigation_message=false})<cr>",
-{ desc = "Previous Hunk" })
+  { desc = "Previous Hunk" })
+vim.keymap.set("n", "<leader>gb", require("telescope.builtin").git_branches, { desc = "Checkout Branch" })
+vim.keymap.set("n", "<leader>gs", "<cmd>G<cr>", { desc = "Status" })
+
+-- l prefix ( Lsp Stuff )
+if util.plugin_loaded("nvim-dap") then
+  wk.register({ d = { name = string.format("%s Debug", icons.Debugger) } })
+  vim.keymap.set("n", "<F5>", function() require("dap").continue() end, { desc = "Debugger: Start" })
+  vim.keymap.set("n", "<F17>", function() require("dap").terminate() end, { desc = "Debugger: Stop" })
+  vim.keymap.set("n", "<F29>", function() require("dap").restart_frame() end, { desc = "Debugger: Restart" })
+  vim.keymap.set("n", "<F6>", function() require("dap").pause() end, { desc = "Debugger: Pause" })
+  vim.keymap.set("n", "<F9>", function() require("dap").toggle_breakpoint() end, { desc = "Debugger: Toggle Breakpoint" })
+  vim.keymap.set("n", "<F10>", function() require("dap").step_over() end, { desc = "Debugger: Step Over" })
+  vim.keymap.set("n", "<F11>", function() require("dap").step_into() end, { desc = "Debugger: Step Into" })
+  vim.keymap.set("n", "<F23>", function() require("dap").step_out() end, { desc = "Debugger: Step Out" })
+  vim.keymap.set("n", "<leader>db", function() require("dap").toggle_breakpoint() end,
+    { desc = "Toggle Breakpoint (F9)" })
+  vim.keymap.set("n", "<leader>dB", function() require("dap").clear_breakpoints() end, { desc = "Clear Breakpoints" })
+  vim.keymap.set("n", "<leader>dc", function() require("dap").continue() end, { desc = "Start/Continue (F5)" })
+  vim.keymap.set("n", "<leader>di", function() require("dap").step_into() end, { desc = "Step Into (F11)" })
+  vim.keymap.set("n", "<leader>do", function() require("dap").step_over() end, { desc = "Step Over (F10)" })
+  vim.keymap.set("n", "<leader>dO", function() require("dap").step_out() end, { desc = "Step Out (S-F11)" })
+  vim.keymap.set("n", "<leader>dq", function() require("dap").close() end, { desc = "Close Session" })
+  vim.keymap.set("n", "<leader>dQ", function() require("dap").terminate() end, { desc = "Terminate Session (S-F5)" })
+  vim.keymap.set("n", "<leader>dp", function() require("dap").pause() end, { desc = "Pause (F6)" })
+  vim.keymap.set("n", "<leader>dr", function() require("dap").restart_frame() end, { desc = "Restart (C-F5)" })
+  vim.keymap.set("n", "<leader>dR", function() require("dap").repl.toggle() end, { desc = "Toggle REPL" })
+  if util.plugin_loaded("nvim-dap-ui") then
+    vim.keymap.set("n", "<leader>du", function() require("dapui").toggle() end, { desc = "Toggle Debugger UI" })
+    vim.keymap.set("n", "<leader>dh", function() require("dap.ui.widgets").hover() end, { desc = "Debugger Hover" })
+  end
+end
+
+-- p prefix (plugin manager)
+vim.keymap.set("n", "<leader>pi", function() require("lazy").install() end, { desc = "Plugins Install" })
+vim.keymap.set("n", "<leader>ps", function() require("lazy").home() end, { desc = "Plugins Status" })
+vim.keymap.set("n", "<leader>pS", function() require("lazy").sync() end, { desc = "Plugins Sync" })
+vim.keymap.set("n", "<leader>pu", function() require("lazy").check() end, { desc = "Plugins Check Updates" })
+vim.keymap.set("n", "<leader>pU", function() require("lazy").update() end, { desc = "Plugins Update" })
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -87,15 +87,15 @@ return {
             ['[]'] = '@class.outer',
           },
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
+        -- swap = {
+        --   enable = true,
+        --   swap_next = {
+        --     ['<leader>a'] = '@parameter.inner',
+        --   },
+        --   swap_previous = {
+        --     ['<leader>A'] = '@parameter.inner',
+        --   },
+        -- },
       },
     }
   end,
