ad745b35cfd21d6a426f6b64ab90033c500358ad
Shad Kaske <shadkaske@gmail.com>
2023-04-17 08:42:12 -0500

    Tinker Terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,10 +5,22 @@ return {
     require("toggleterm").setup()
 
     local Terminal = require("toggleterm.terminal").Terminal
+
     local lazygit = Terminal:new({
       cmd = "lazygit",
       dir = "git_dir",
       direction = "float",
+      float_opts = {
+        border = "double",
+      },
+      hidden = true
+    })
+
+    local tinker = Terminal:new({
+      cmd = "php artisan tinker",
+      dir = "git_dir",
+      direction = "horizontal",
+      size = 50,
       hidden = true
     })
 
@@ -16,10 +28,20 @@ return {
       lazygit:toggle()
     end
 
+    function _tinker_toggle()
+      tinker:toggle()
+    end
+
     vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua _lazygit_toggle()<cr>", {
       noremap = true,
       silent = true,
       desc = "LazyGit",
+    })
+
+    vim.api.nvim_set_keymap("n", "<leader>ua", "<cmd>lua _tinker_toggle()<cr>", {
+      noremap = true,
+      silent = true,
+      desc = "Tinker",
     })
   end
 }
