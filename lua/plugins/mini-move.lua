de23fc26c72fb566184c67d0e4a007ba673ab739
Shad Kaske <shadkaske@gmail.com>
2023-04-17 09:04:04 -0500

    Changed mini.move keybinds

--- lua/plugins/mini-move.lua
+++ lua/plugins/mini-move.lua
@@ -2,6 +2,22 @@ return {
   'echasnovski/mini.move',
   version = '*',
   config = function ()
-    require('mini.move').setup()
+    require('mini.move').setup({
+      mappings = {
+        -- Visual Selection
+        up = '<C-Up>',
+        down = '<C-Down>',
+        left = '<C-Left>',
+        right = '<C-Right>',
+        -- Current line in Normal Mode
+        line_up = '<C-Up>',
+        line_down = '<C-Down>',
+        line_left = '<C-Left>',
+        line_right = '<C-Right>',
+      },
+      options = {
+        reindent_linewise = true,
+      },
+    })
   end,
 }
