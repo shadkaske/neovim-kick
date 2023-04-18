f0b1a53f7915a7c39a1730ba7b0dc194bafb2d99
Shad Kaske <shadkaske@gmail.com>
2023-04-18 06:23:03 -0500

    Lazy Load Todo Comments

--- lua/plugins/todo-commments.lua
+++ lua/plugins/todo-commments.lua
@@ -1,13 +1,17 @@
 return {
   "folke/todo-comments.nvim",
-  cmd = { "TodoTrouble", "TodoTelescope" },
+  lazy = true,
+  cmd = {
+    "TodoTrouble",
+    "TodoTelescope",
+    "TodoLocList",
+    "TodoQuickFix",
+  },
   event = { "BufReadPost", "BufNewFile" },
   config = true,
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-    { "<leader>tt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-    { "<leader>tT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-    { "<leader>tt", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>lt", "<cmd>TodoTelescope<cr>", desc = "Todo" },
   },
 }
