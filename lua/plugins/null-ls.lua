8c845af32649cfdfbd4cece02327068c6ace74d9
Shad Kaske <shadkaske@gmail.com>
2023-04-24 16:03:35 -0500

    Null-Ls Added

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,17 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  config = function ()
+    local formatting = require("null-ls").builtins.formatting
+    -- Can use this if you need diagnostics
+    -- local diagnostics = require("null-ls").builtins.diagnostics
+
+    require("null-ls").setup({
+      debug = false,
+      sources = {
+        formatting.prettier,
+        formatting.stylua,
+        formatting.blade_formatter,
+      }
+    })
+  end
+}
