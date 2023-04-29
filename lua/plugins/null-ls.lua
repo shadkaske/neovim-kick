d5f639b5f43936b24097f41783962eec58c41d62
Shad Kaske <shadkaske@gmail.com>
2023-04-29 06:52:03 -0500

    Mason Null Ls format installer

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,11 +1,17 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  config = function ()
-    local formatting = require("null-ls").builtins.formatting
+  'jose-elias-alvarez/null-ls.nvim',
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    'williamboman/mason.nvim',
+    'jay-babu/mason-null-ls.nvim',
+  },
+  config = function()
+    require("mason").setup()
+    local formatting = require('null-ls').builtins.formatting
     -- Can use this if you need diagnostics
     -- local diagnostics = require("null-ls").builtins.diagnostics
 
-    require("null-ls").setup({
+    require('null-ls').setup {
       debug = false,
       sources = {
         formatting.prettier,
@@ -14,7 +20,12 @@ return {
         formatting.xmlformat,
         formatting.sql_formatter,
         formatting.shfmt,
-      }
+      },
+    }
+
+    require("mason-null-ls").setup({
+      ensure_installed = {},
+      automatic_installation = true,
     })
-  end
+  end,
 }
