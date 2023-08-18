5476bb55444285b5e3b6bd40bc0602e418c4a2ba
Shad Kaske <shadkaske@gmail.com>
2023-08-18 10:42:50 -0500

    nvim-tree save size on open

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,20 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
   lazy = false,
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require("nvim-tree").setup {}
+    require('nvim-tree').setup {
+      actions = {
+        open_file = {
+          resize_window = false,
+        },
+        view = {
+          preserve_window_proportions = false,
+        },
+      },
+    }
   end,
 }
