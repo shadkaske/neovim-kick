321ead98bddc6beae1e932046d5fcc71eafb626a
Shad Kaske <shadkaske@gmail.com>
2023-08-22 10:18:35 -0500

    Icon FIxes

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -69,14 +69,14 @@ return {
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
+          pause = '',
+          play = '󰐊',
+          step_into = '',
+          step_over = '',
+          step_out = '󰆸',
+          step_back = '',
+          run_last = '󰐊󰐊',
+          terminate = '',
         },
       },
     }
