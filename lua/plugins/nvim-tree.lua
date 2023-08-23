2f9546e046ba1733eec8ed572882920b9196f67c
Shad Kaske <shadkaske@gmail.com>
2023-08-23 09:03:48 -0500

    Nvim Tree Enhancements

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,36 @@ return {
   },
   config = function()
     require('nvim-tree').setup {
+      disable_netrw = true,
+      view = {
+        width = '20%',
+        side = 'left',
+      },
+      update_focused_file = {
+        enable = true,
+      },
+      diagnostics = {
+        enable = false,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+        debounce_delay = 50,
+        severity = {
+          min = vim.diagnostic.severity.HINT,
+          max = vim.diagnostic.severity.ERROR,
+        },
+        icons = {
+          hint = '',
+          info = '',
+          warning = '',
+          error = '',
+        },
+      },
+      filters = {
+        git_ignored = false,
+        custom = {
+          "^.null-ls.*",
+        },
+      },
       actions = {
         open_file = {
           resize_window = false,
