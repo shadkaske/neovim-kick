d5ffe5749b07e4a57ac9ca2b6f29fafe2251bab8
Shad Kaske <shadkaske@gmail.com>
2023-08-24 13:10:45 -0500

    Add an ignore to todo-comment
    Mostly to cover a case where I have a python virtual environment and don't want to
    see all of the Todos from the imported python code

--- lua/plugins/todo-commments.lua
+++ lua/plugins/todo-commments.lua
@@ -8,7 +8,21 @@ return {
     "TodoQuickFix",
   },
   event = { "BufReadPost", "BufNewFile" },
-  config = true,
+  config = function ()
+    require('todo-comments').setup {
+      search = {
+        command = "rg",
+        args = {
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--glob=!lib/"
+        },
+      },
+    }
+  end,
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
