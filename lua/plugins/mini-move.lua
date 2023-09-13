d378c3493ff249e2c3ad96d9b8d5a0324fd07082
Shad Kaske <shadkaske@gmail.com>
2023-09-13 08:05:46 -0500

    Changed Mini Move Keymaps

--- lua/plugins/mini-move.lua
+++ lua/plugins/mini-move.lua
@@ -5,15 +5,10 @@ return {
     require('mini.move').setup({
       mappings = {
         -- Visual Selection
-        up = '<C-Up>',
-        down = '<C-Down>',
-        left = '<C-Left>',
-        right = '<C-Right>',
-        -- Current line in Normal Mode
-        line_up = '<C-Up>',
-        line_down = '<C-Down>',
-        line_left = '<C-Left>',
-        line_right = '<C-Right>',
+        up = '<C-S-k>',
+        down = '<C-S-j>',
+        left = '<C-S-h>',
+        right = '<C-S-l>',
       },
       options = {
         reindent_linewise = true,
