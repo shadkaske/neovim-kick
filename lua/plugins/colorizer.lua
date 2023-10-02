8a2cd07336a8fa079aa8eb27078e9a9267bf10d7
Shad Kaske <shadkaske@gmail.com>
2023-10-02 09:02:02 -0500

    Added Nvim Colorizer

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,10 @@
+return {
+  "norcalli/nvim-colorizer.lua",
+  lazy = true,
+  cmd = {
+    "ColorizerAttachToBuffer",
+    "ColorizerDetachFromBuffer",
+    "ColorizerReloadAllBuffers",
+    "ColorizerToggle",
+  },
+}
