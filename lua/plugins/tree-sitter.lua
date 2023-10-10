75a0883d660f683d533e66c7180d93609e80aeed
Shad Kaske <shadkaske@gmail.com>
2023-10-10 09:08:51 -0500

    TreeSitter Update

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
-  build = ":TSUpdate",
+  build = ':TSUpdate',
   opts = function()
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
@@ -13,6 +13,7 @@ return {
         'c',
         'cpp',
         'css',
+        'dockerfile',
         'git_rebase',
         'gitattributes',
         'gitcommit',
@@ -29,15 +30,15 @@ return {
         'regex',
         'rust',
         'sql',
+        'toml',
         'tsx',
         'typescript',
-        'vimdoc',
         'vim',
+        'vimdoc',
         'vue',
         'yaml',
       },
 
-      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = true,
 
       highlight = {
