93fe8b78935d204551aba951f18d7d28488d43c5
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-10 14:16:34 -0600

    Cleaning up my xdebug config

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,15 +1,5 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
@@ -25,18 +15,9 @@ return {
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_setup = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
       handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
         'php',
       },
     }
@@ -44,7 +25,7 @@ return {
     dap.adapters.php = {
       type = 'executable',
       command = 'node',
-      args = { '/home/kaskshad/Code/sources/vscode-php-debug/out/phpDebug.js' },
+      args = { os.getenv("HOME") .. "/Code/sources/vscode-php-debug/out/phpDebug.js" },
     }
 
     dap.configurations.php = {
@@ -55,27 +36,12 @@ return {
         port = '9003',
         log = true,
       },
-      -- {
-      --   type = 'php',
-      --   request = 'launch',
-      --   name = 'Listen for Xdebug - Sail',
-      --   port = '9003',
-      --   pathMappings = {
-      --     ['/var/www/html'] = '${workspaceFolder}',
-      --   },
-      --   log = true,
-      -- },
     }
 
     vim.fn.sign_define('DapBreakpoint', { text = '󰯯', texthl = 'ErrorMsg', linehl = '', numhl = '' })
     vim.fn.sign_define('DapBreakpointCondition', { text = '󰯲', texthl = 'WarningMsg', linehl = '', numhl = '' })
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
@@ -94,7 +60,5 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- Install golang specific config
   end,
 }
