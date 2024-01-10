7d113f1c007066ef8a8f135931d2a54ec9727aef
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-10 14:17:30 -0600

    Updated laravel plugin config

--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -1,27 +1,33 @@
 return {
-  "adalessa/laravel.nvim",
+  'adalessa/laravel.nvim',
   dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "tpope/vim-dotenv",
-    "MunifTanjim/nui.nvim",
+    'nvim-telescope/telescope.nvim',
+    'tpope/vim-dotenv',
+    'MunifTanjim/nui.nvim',
+    'nvimtools/none-ls.nvim',
   },
-  cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+  cmd = { 'Sail', 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
   keys = {
-    -- { "<leader>la", ":Laravel artisan<cr>" },
-    -- { "<leader>lr", ":Laravel routes<cr>" },
-    -- { "<leader>lm", ":Laravel related<cr>" },
-    -- {
-    --   "<leader>lt",
-    --   function()
-    --     require("laravel.tinker").send_to_tinker()
-    --   end,
-    --   mode = "v",
-    --   desc = "Laravel Application Routes",
-    -- },
+    { "<leader>aa", ":Laravel artisan<cr>" },
+    { "<leader>ar", ":Laravel routes<cr>" },
+    { "<leader>am", ":Laravel related<cr>" },
+    {
+      "<leader>at",
+      function()
+        require("laravel.tinker").send_to_tinker()
+      end,
+      mode = "v",
+      desc = "Laravel Application Routes",
+    },
   },
-  event = { "VeryLazy" },
+  event = { 'VeryLazy' },
+  features = {
+    null_ls = {
+      enable = true,
+    },
+  },
   config = function()
-    require("laravel").setup()
-    require("telescope").load_extension "laravel"
+    require('laravel').setup()
+    require('telescope').load_extension 'laravel'
   end,
 }
