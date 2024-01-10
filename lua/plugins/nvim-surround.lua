2303af4c2fc63558cb957c1a13a3ed6a95df0172
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-10 14:17:48 -0600

    Changed to using mini-surround

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,7 +1,7 @@
 return {
-  'kylechui/nvim-surround',
-  version = '*', -- Use for stability; omit to use `main` branch for the latest features
-  event = 'VeryLazy',
-  opts = {},
-  config = function(_, opts) require("nvim-surround").setup(opts) end,
+  -- 'kylechui/nvim-surround',
+  -- version = '*', -- Use for stability; omit to use `main` branch for the latest features
+  -- event = 'VeryLazy',
+  -- opts = {},
+  -- config = function(_, opts) require("nvim-surround").setup(opts) end,
 }
