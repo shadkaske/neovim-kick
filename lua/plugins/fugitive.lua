fc1e917faaa731abc404e6f3d5a9df414767c3b2
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-13 09:35:57 -0600

    Removed telescope fzf and added git remove keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 return {
-  -- Git related plugins
   'tpope/vim-fugitive',
   keys = {
     { '<leader>gc', '<cmd>G commit<cr>', desc = 'Git Commit' },
     { '<leader>gg', '<cmd>G<cr>', desc = 'Fugitive Status' },
+    { '<leader>gX', '<cmd>G rm %<cr>', desc = 'Remove File' },
   }
 }
