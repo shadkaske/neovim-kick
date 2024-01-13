0dfe06ba72e6c12fab3d32bf89d0956bd04dc5ba
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-13 08:46:55 -0600

    Moved debug keymaps to plugin spec

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -24,9 +24,7 @@ return {
 
     dap.adapters.php = {
       type = 'executable',
-      command = vim.fn.stdpath("data") .. "/mason/bin/php-debug-adapter"
-      -- command = 'node',
-      -- args = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js" },
+      command = vim.fn.stdpath 'data' .. '/mason/bin/php-debug-adapter',
     }
 
     dap.configurations.php = {
@@ -61,5 +59,70 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    -- d prefix ( Debug Stuff )
+    vim.keymap.set('n', '<F5>', function()
+      require('dap').continue()
+    end, { desc = 'Debugger: Start' })
+    vim.keymap.set('n', '<F17>', function()
+      require('dap').terminate()
+    end, { desc = 'Debugger: Stop' })
+    vim.keymap.set('n', '<F29>', function()
+      require('dap').restart_frame()
+    end, { desc = 'Debugger: Restart' })
+    vim.keymap.set('n', '<F6>', function()
+      require('dap').pause()
+    end, { desc = 'Debugger: Pause' })
+    vim.keymap.set('n', '<F9>', function()
+      require('dap').toggle_breakpoint()
+    end, { desc = 'Debugger: Toggle Breakpoint' })
+    vim.keymap.set('n', '<F10>', function()
+      require('dap').step_over()
+    end, { desc = 'Debugger: Step Over' })
+    vim.keymap.set('n', '<F11>', function()
+      require('dap').step_into()
+    end, { desc = 'Debugger: Step Into' })
+    vim.keymap.set('n', '<F23>', function()
+      require('dap').step_out()
+    end, { desc = 'Debugger: Step Out' })
+    vim.keymap.set('n', '<leader>db', function()
+      require('dap').toggle_breakpoint()
+    end, { desc = 'Toggle Breakpoint (F9)' })
+    vim.keymap.set('n', '<leader>dB', function()
+      require('dap').clear_breakpoints()
+    end, { desc = 'Clear Breakpoints' })
+    vim.keymap.set('n', '<leader>dc', function()
+      require('dap').continue()
+    end, { desc = 'Start/Continue (F5)' })
+    vim.keymap.set('n', '<leader>di', function()
+      require('dap').step_into()
+    end, { desc = 'Step Into (F11)' })
+    vim.keymap.set('n', '<leader>do', function()
+      require('dap').step_over()
+    end, { desc = 'Step Over (F10)' })
+    vim.keymap.set('n', '<leader>dO', function()
+      require('dap').step_out()
+    end, { desc = 'Step Out (S-F11)' })
+    vim.keymap.set('n', '<leader>dq', function()
+      require('dap').close()
+    end, { desc = 'Close Session' })
+    vim.keymap.set('n', '<leader>dQ', function()
+      require('dap').terminate()
+    end, { desc = 'Terminate Session (S-F5)' })
+    vim.keymap.set('n', '<leader>dp', function()
+      require('dap').pause()
+    end, { desc = 'Pause (F6)' })
+    vim.keymap.set('n', '<leader>dr', function()
+      require('dap').restart_frame()
+    end, { desc = 'Restart (C-F5)' })
+    vim.keymap.set('n', '<leader>dR', function()
+      require('dap').repl.toggle()
+    end, { desc = 'Toggle REPL' })
+    vim.keymap.set('n', '<leader>du', function()
+      require('dapui').toggle()
+    end, { desc = 'Toggle Debugger UI' })
+    vim.keymap.set('n', '<leader>dh', function()
+      require('dap.ui.widgets').hover()
+    end, { desc = 'Debugger Hover' })
   end,
 }
