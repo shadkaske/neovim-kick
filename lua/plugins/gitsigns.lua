15404aec82419faf1874ca3cc9fa0e04dd9a115e
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-13 09:28:38 -0600

    Added stage file keymap

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -15,6 +15,7 @@ return {
     { '<leader>gP', "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", desc = 'Preview Hunk' },
     { '<leader>gR', "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", desc = 'Reset Buffer' },
     { '<leader>gd', '<cmd>Gitsigns diffthis HEAD<cr>', desc = 'Git Diff' },
+    { '<leader>gf', '<cmd>Gitsigns stage_buffer<cr>', desc = 'Stage File' },
     { '<leader>gn', "<cmd>lua require('gitsigns').next_hunk({navigation_message=false})<cr>", desc = 'Next Hunk' },
     { '<leader>gp', "<cmd>lua require('gitsigns').prev_hunk({navigation_message=false})<cr>", desc = 'Previous Hunk' },
     { '<leader>gr', "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", desc = 'Reset Hunk' },
