5cd6540181404a874f8e542c6632dc4c17c016aa
Shad Kaske <Shad.Kaske@showplacecabinetry.com>
2024-01-14 08:08:18 -0600

    Updated fugitive git rm shortcut

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,6 @@ return {
   keys = {
     { '<leader>gc', '<cmd>G commit<cr>', desc = 'Git Commit' },
     { '<leader>gg', '<cmd>G<cr>', desc = 'Fugitive Status' },
-    { '<leader>gX', '<cmd>G rm %<cr>', desc = 'Remove File' },
+    { '<leader>gX', '<cmd>G rm % | Bdelete!<cr>', desc = 'Remove File' },
   }
 }
