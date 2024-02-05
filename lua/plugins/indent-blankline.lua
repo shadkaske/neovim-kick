f424be871116fe2c93b014e3ab2a3c66f9daf13b
Shad Kaske <shadkaske@gmail.com>
2024-02-05 16:10:08 -0600

    orgmode tweaks

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -14,6 +14,7 @@ return {
       filetypes = {
         'dashboard',
         'alpha',
+        'org',
       },
     },
   },
--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -5,11 +5,20 @@ return {
     'nvim-treesitter/nvim-treesitter',
     'akinsho/org-bullets.nvim',
   },
-  config = function ()
+  config = function()
     require('orgmode').setup_ts_grammar()
-    require('orgmode').setup({
-      org_agenda_files = {'~/org/*'},
-      org_default_notes_file = '~/org/refile.org'
-    })
-  end
+
+    require('nvim-treesitter.configs').setup {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'org' },
+      },
+      ensure_installed = { 'org' },
+    }
+
+    require('orgmode').setup {
+      org_agenda_files = { '~/org/*' },
+      org_default_notes_file = '~/org/refile.org',
+    }
+  end,
 }
