a4d1aa4dd89a9eab482ca8ea9da8a3b47209f7e8
Shad Kaske <shadkaske@gmail.com>
2024-02-05 16:09:38 -0600

    Added some fugitive commands to plugin spec

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,10 @@ return {
   'tpope/vim-fugitive',
   cmd = {
     "G",
+    "Git",
+    "Gstatus",
+    "Gdiffsplit",
+    "Gvdiffsplit",
   },
   keys = {
     { '<leader>gc', '<cmd>G commit<cr>', desc = 'Git Commit' },
