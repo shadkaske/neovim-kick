307b3101493a4d0104d42141f1b9e1047093253d
Shad Kaske <shadkaske@gmail.com>
2024-02-14 14:26:36 -0600

    Onedark variants

--- lua/plugins/colorscheme-onedarkpro.lua
+++ lua/plugins/colorscheme-onedarkpro.lua
@@ -4,9 +4,9 @@ return {
   lazy = false,
   config = function()
     require('onedarkpro').setup({})
-    -- vim.cmd.colorscheme "onedark"
+    vim.cmd.colorscheme "onedark"
     -- vim.cmd.colorscheme "onelight"
-    vim.cmd.colorscheme "onedark_vivid"
+    -- vim.cmd.colorscheme "onedark_vivid"
     -- vim.cmd.colorscheme "onedark_dark"
   end,
 }
