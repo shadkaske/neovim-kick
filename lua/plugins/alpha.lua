ad6cb858d2d7e6aa8eac3a3566b22e889c223575
Shad Kaske <shadkaske@gmail.com>
2024-02-15 12:50:31 -0600

    Updated to a more subtle alpha logo

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -4,17 +4,21 @@ return {
     event = 'VimEnter',
     opts = function()
       local dashboard = require 'alpha.themes.dashboard'
-      local logo = [[
-                                             
-      ████ ██████           █████      ██
-     ███████████             █████ 
-     █████████ ███████████████████ ███   ███████████
-    █████████  ███    █████████████ █████ ██████████████
-   █████████ ██████████ █████████ █████ █████ ████ █████
- ███████████ ███    ███ █████████ █████ █████ ████ █████
-██████  █████████████████████ ████ █████ █████ ████ ██████
-]]
-      dashboard.section.header.val = vim.split(logo, '\n')
+
+      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo1', { fg = '#61afef' })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo2', { fg = '#89ca78', bg = '#61afef' })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo3', { fg = '#89ca78' })
+
+      dashboard.section.header.val = {
+        [[     █  █     ]],
+        [[     ██ ██     ]],
+        [[     █████     ]],
+        [[     ██ ███     ]],
+        [[     █  █     ]],
+        [[]],
+        [[N  E  O   V  I  M]],
+      }
+
       dashboard.section.buttons.val = {
         dashboard.button('f', ' ' .. ' Find file', ':Telescope find_files <CR>'),
         dashboard.button('r', ' ' .. ' Recent files', ':Telescope oldfiles <CR>'),
@@ -24,7 +28,15 @@ return {
         dashboard.button('l', '󰒲 ' .. ' Lazy', ':Lazy<CR>'),
         dashboard.button('q', ' ' .. ' Quit', ':qa<CR>'),
       }
-      dashboard.section.header.opts.hl = 'AlphaHeader'
+
+      -- dashboard.section.header.opts.hl = 'AlphaHeader'
+      dashboard.section.header.opts.hl = {
+        { { 'NeovimDashboardLogo1', 6, 8 }, { 'NeovimDashboardLogo3', 9, 22 } },
+        { { 'NeovimDashboardLogo1', 6, 8 }, { 'NeovimDashboardLogo2', 9, 11 }, { 'NeovimDashboardLogo3', 12, 24 } },
+        { { 'NeovimDashboardLogo1', 6, 11 }, { 'NeovimDashboardLogo3', 12, 26 } },
+        { { 'NeovimDashboardLogo1', 6, 11 }, { 'NeovimDashboardLogo3', 12, 24 } },
+        { { 'NeovimDashboardLogo1', 6, 11 }, { 'NeovimDashboardLogo3', 12, 22 } },
+      }
       dashboard.opts.layout[1].val = 6
       return dashboard
     end,
