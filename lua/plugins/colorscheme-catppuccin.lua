639e52a70107432d82d0f6d5f1496a008c768241
Shad Kaske <shadkaske@gmail.com>
2024-02-15 12:33:38 -0600

    Catppuccin in separate file

--- lua/plugins/colorscheme-catppuccin.lua
+++ lua/plugins/colorscheme-catppuccin.lua
@@ -1,34 +1,37 @@
 return {
-  -- "catppuccin/nvim",
-  -- priority = 1000,
-  -- name = "catppuccin",
-  -- opts = {
-  --   flavor = "mocha",
-  --   integrations = {
-  --     aerial = true,
-  --     cmp = true,
-  --     fidget = true,
-  --     gitsigns = true,
-  --     indent_blankline = {
-  --       enabled = true,
-  --       colored_indent_levels = false,
-  --     },
-  --     leap = true,
-  --     mason = true,
-  --     mini = true,
-  --     navic = {
-  --       enabled = true,
-  --       custom_bg = "#181826",
-  --     },
-  --     nvimtree = true,
-  --     noice = true,
-  --     notify = true,
-  --     telescope = true,
-  --     treesitter = true,
-  --     treesitter_context = true,
-  --     lsp_trouble = true,
-  --     window_picker = true,
-  --     which_key = true,
-  --   },
-  -- }
+  "catppuccin/nvim",
+  priority = 1000,
+  name = "catppuccin",
+  opts = {
+    flavor = "mocha",
+    integrations = {
+      aerial = true,
+      cmp = true,
+      fidget = true,
+      gitsigns = true,
+      indent_blankline = {
+        enabled = true,
+        colored_indent_levels = false,
+      },
+      leap = true,
+      mason = true,
+      mini = true,
+      navic = {
+        enabled = true,
+        custom_bg = "#181826",
+      },
+      nvimtree = true,
+      noice = true,
+      notify = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      lsp_trouble = true,
+      window_picker = true,
+      which_key = true,
+    },
+  },
+  config = function ()
+    -- vim.cmd.colorscheme 'catppuccin'
+  end
 }
