34381c89aadb5eb967ac9cad70a4185ade2540ae
Shad Kaske <shadkaske@gmail.com>
2024-03-04 15:47:48 -0600

    Reformat on Autocommands

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -10,26 +10,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Automagically Trim Whitespace
-vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*" }, command = [[%s/\s\+$//e]] })
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, { pattern = { '*' }, command = [[%s/\s\+$//e]] })
 
 -- Turn of diagnostics for .env files
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { ".env", ".env.example" },
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = { '.env', '.env.example' },
   command = [[lua vim.diagnostic.disable()]],
 })
 
 -- Run npm build on stupid vite vue projects
-vim.api.nvim_create_autocmd(
-  { "BufWritePost" },
-  { pattern = { "**/editracker/*/*.vue" },
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  pattern = { '**/editracker/*/*.vue' },
   command = [[!yarn run build &]],
 })
 
 -- Set Btxml Files to xml filetype
-vim.api.nvim_create_autocmd(
-  { "BufWritePre", "BufNewFile" },
-  { pattern = { "*.btxml" }, command = [[set filetype=xml]],
-})
+vim.api.nvim_create_autocmd({ 'BufWritePre', 'BufNewFile' }, { pattern = { '*.btxml' }, command = [[set filetype=xml]] })
 
 -- Set Lsp Mappings
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -46,22 +42,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>cD', vim.lsp.buf.type_definition, { desc = 'Type Definition'})
+    vim.keymap.set('n', '<space>cD', vim.lsp.buf.type_definition, { desc = 'Type Definition' })
     vim.keymap.set('n', '<space>cr', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
   end,
 })
 
 -- Close if nvimtree is the last man standing
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd('BufEnter', {
   nested = true,
   callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-      vim.cmd "quit"
+    if #vim.api.nvim_list_wins() == 1 and require('nvim-tree.utils').is_nvim_tree_buf() then
+      vim.cmd 'quit'
     end
-  end
+  end,
 })
