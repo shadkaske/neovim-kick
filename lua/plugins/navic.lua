80e3aaa818199fb43c3bd709a1466a52f0b1cf12
Shad Kaske <shadkaske@gmail.com>
2024-03-04 15:48:20 -0600

    Disabled Navic

--- lua/plugins/navic.lua
+++ lua/plugins/navic.lua
@@ -1,26 +1,26 @@
 return {
-  'SmiteshP/nvim-navic',
-  init = function()
-    vim.g.navic_silence = true
-    require('util').on_attach(function(client, buffer)
-      if client.server_capabilities.documentSymbolProvider then
-        require('nvim-navic').attach(client, buffer)
-      end
-    end)
-  end,
-  opts = function()
-    local icons = require 'util.icons'
-    return {
-      separator = string.format(' %s  ', icons.DoubleChevronRight),
-      highlight = true,
-      depth_limit = 4,
-      depth_limit_indicator = icons.Ellipsis,
-      icons = icons.kinds,
-    }
-  end,
-  config = function()
-    require('nvim-navic').setup {
-      highlight = true,
-    }
-  end,
+  -- 'SmiteshP/nvim-navic',
+  -- init = function()
+  --   vim.g.navic_silence = true
+  --   require('util').on_attach(function(client, buffer)
+  --     if client.server_capabilities.documentSymbolProvider then
+  --       require('nvim-navic').attach(client, buffer)
+  --     end
+  --   end)
+  -- end,
+  -- opts = function()
+  --   local icons = require 'util.icons'
+  --   return {
+  --     separator = string.format(' %s  ', icons.DoubleChevronRight),
+  --     highlight = true,
+  --     depth_limit = 4,
+  --     depth_limit_indicator = icons.Ellipsis,
+  --     icons = icons.kinds,
+  --   }
+  -- end,
+  -- config = function()
+  --   require('nvim-navic').setup {
+  --     highlight = true,
+  --   }
+  -- end,
 }
